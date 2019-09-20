4620b483077cc3263a8aca3ef5a88adf82cf391a
Tami Williams <tami@asktami.com>
2019-09-20 18:32:51 -0400

    Updates

---
+++ precache-manifest.160b965eb61534a566b6a97a52f7a00c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ff482836ac8e40ba09d917eea37c1b",
+    "url": "/react-video-player/index.html"
+  },
+  {
+    "revision": "35ee0c1626ca7d2220b6",
+    "url": "/react-video-player/static/css/main.de4db88c.chunk.css"
+  },
+  {
+    "revision": "15b0735633ad9faaaf35",
+    "url": "/react-video-player/static/js/2.888d38f2.chunk.js"
+  },
+  {
+    "revision": "35ee0c1626ca7d2220b6",
+    "url": "/react-video-player/static/js/main.ce7c8a51.chunk.js"
+  },
+  {
+    "revision": "02a5a78ac3469f74557d",
+    "url": "/react-video-player/static/js/runtime-main.d19bb231.js"
+  }
+]);
